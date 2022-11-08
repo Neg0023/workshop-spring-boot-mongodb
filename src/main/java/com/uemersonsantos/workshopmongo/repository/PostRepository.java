39f317afcf5345af42002275027baded508dadbe
Uemerson <uemerson161@hotmail.com>
2022-11-07 21:39:17 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/uemersonsantos/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.uemersonsantos.workshopmongo.repository;
+
+import com.uemersonsantos.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post,String> {
+
+}
